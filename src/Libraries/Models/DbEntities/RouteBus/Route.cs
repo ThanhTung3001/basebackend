ae38e62e5c75d3699b9c5799bfef47409f0e9cc8
ThanhTung3001 <tungle3001.itfreelancer@gmail.com>
2023-05-10 09:01:19 +0700

    commit

--- src/Libraries/Models/DbEntities/RouteBus/Route.cs
+++ src/Libraries/Models/DbEntities/RouteBus/Route.cs
@@ -9,10 +9,15 @@ public class Route : BaseEntity
     public string Description { get; set; }
     public string StartLocation { get; set; }
     public string EndLocation { get; set; }
+
     public List<RouteStop> RouteStops { get; set; }
+
     public List<RouteVehicle> RouteVehicles { get; set; }
+
     public List<RouteSchedule> RouteSchedules { get; set; }
+
     public string TransportationType { get; set; }
+
     public decimal BaseFare { get; set; }
 
     public int Code { get; set; }
